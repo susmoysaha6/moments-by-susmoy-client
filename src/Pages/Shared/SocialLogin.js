9b55f62370835c1388f22d185e2323481670cc20
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-08 01:06:10 +0600

    Google sign in implemented

--- src/Pages/Shared/SocialLogin.js
+++ src/Pages/Shared/SocialLogin.js
@@ -1,11 +1,29 @@
 import { Button } from 'flowbite-react';
-import React from 'react';
+
+import React, { useContext } from 'react';
 import { FcGoogle } from "react-icons/fc";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const SocialLogin = () => {
+    const { googleSignIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
+
+    const handleGoogleLogin = () => {
+        googleSignIn()
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate(from, { replace: true });
+            })
+
+    }
+
     return (
         <div className='mb-2' >
-            <Button gradientMonochrome="success" className=' w-full'><FcGoogle className='mr-1' />  Signin With Google </Button>
+            <Button onClick={handleGoogleLogin} gradientMonochrome="success" className=' w-full'><FcGoogle className='mr-1' />  Signin With Google </Button>
         </div >
     );
 };

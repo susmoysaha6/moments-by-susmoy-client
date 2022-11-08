487b18c1730df8c1f6face54818c30db7d59ed2c
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-08 20:43:26 +0600

    services section added

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,13 +1,20 @@
-import { useTheme } from 'flowbite-react';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import useTitle from '../../hooks/useTitle';
+import ServiceCard from '../Shared/ServiceCard';
 
 const Services = () => {
-    useTheme('dark')
     useTitle('Services')
+    const services = useLoaderData();
+    console.log(services);
     return (
         <div>
-            service
+            <p className='my-5 text-3xl text-center text-blue-700 font-bold'>All Services By Me</p>
+            <div className='w-3/4 mx-auto my-10 grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -32,7 +32,8 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/services',
-                element: <Services></Services>
+                element: <Services></Services>,
+                loader: () => fetch('http://localhost:5000/services')
             },
             {
                 path: '/blogs',

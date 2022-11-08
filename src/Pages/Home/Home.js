0533b77263fd77ebc0b297764657c41d7d837104
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-08 19:16:39 +0600

    banner section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,27 @@
+
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
+import logo from '../../assets/logo.png'
+import reception from '../../assets/banner/reciption.png'
+import { Button } from 'flowbite-react';
+
 
 const Home = () => {
     useTitle('Home')
     return (
         <div>
-            this is home
+            {/* banner Section*/}
+            <div className='w-full mx-auto flex flex-col items-start md:items-center lg:flex-row justify-center '>
+                <div>
+                    <img src={reception} alt="" />
+                </div>
+                <div className='my-5 ml-5'>
+                    <img width={100} src={logo} alt="" />
+                    <h1 className='md:text-3xl text-slate-700 font-bold flex'>Welcome <br /> to <br /> "MOMENTS BY SUSMOY"</h1>
+                    <p className='text-slate-700 text-lg'>I capture your dream moments</p>
+                    <Button gradientMonochrome='info' className='my-5' >Book Now</Button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     }
 
     return (
-        <div className="px-4 mb-12 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 sticky top-0 bg-white shadow-2xl text-slate-700  z-50 h-fit">
+        <div className="px-4 mb-12 py-5 mx-auto sm:max-w-xl md:max-w-full  md:px-24 lg:px-8 sticky top-0 bg-white shadow-2xl text-slate-700  z-50 h-fit">
             <div className="relative flex items-center justify-between">
                 <Link
                     to="/"
